d21b7fb44bf1c502dcba7ce8083363c6a2bdf90e
ahabib1980 <72734018+ahabib1980@users.noreply.github.com>
2020-12-15 17:12:24 +0500

    controller work mstarted

--- omod/src/main/java/org/openmrs/module/archival/web/controller/ArchivalController.java
+++ omod/src/main/java/org/openmrs/module/archival/web/controller/ArchivalController.java
@@ -15,14 +15,16 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.User;
-import org.openmrs.api.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.archival.api.ArchivalService;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.ModelMap;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * This class configured as controller using annotation and mapped with the URL of
@@ -35,53 +37,17 @@ public class ArchivalController {
 	/** Logger for this class and subclasses */
 	protected final Log log = LogFactory.getLog(getClass());
 	
-	@Autowired
-	UserService userService;
-	
 	/** Success form view name */
-	private final String VIEW = "/module/${rootArtifactid}/${rootArtifactid}";
+	private final String SUCCESS_AND_VIEW = "/module/archival/archive";
 	
-	/**
-	 * Initially called after the getUsers method to get the landing form name
-	 * 
-	 * @return String form view name
-	 */
-	@RequestMapping(method = RequestMethod.GET)
-	public String onGet() {
-		return VIEW;
-	}
+	ArchivalService archivalService;
 	
-	/**
-	 * All the parameters are optional based on the necessity
-	 * 
-	 * @param httpSession
-	 * @param anyRequestObject
-	 * @param errors
-	 * @return
-	 */
-	@RequestMapping(method = RequestMethod.POST)
-	public String onPost(HttpSession httpSession, @ModelAttribute("anyRequestObject") Object anyRequestObject,
-	        BindingResult errors) {
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/module/archival/archival.form")
+	public String showForm(ModelMap model) {
+		archivalService = Context.getService(ArchivalService.class);
 		
-		if (errors.hasErrors()) {
-			// return error view
-		}
 		
-		return VIEW;
+		return SUCCESS_AND_VIEW;
 	}
-	
-	/**
-	 * This class returns the form backing object. This can be a string, a boolean, or a normal java
-	 * pojo. The bean name defined in the ModelAttribute annotation and the type can be just defined
-	 * by the return type of this method
-	 */
-	@ModelAttribute("users")
-	protected List<User> getUsers() throws Exception {
-		List<User> users = userService.getAllUsers();
-		
-		// this object will be made available to the jsp page under the variable name
-		// that is defined in the @ModuleAttribute tag
-		return users;
-	}
-	
 }
